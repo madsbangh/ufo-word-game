8d98d315767ff424bfa56a935daadc936cdfff64
Mads <madsbangh@gmail.com>
2022-02-22 21:49:08 +0100

    Refactor: Create VectorExtensions.ToWorldPosition to convert from board position

---
+++ Assets/Scripts/VectorExtensions.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public static class VectorExtensions
+{
+	public static Vector3 ToWorldPosition(this Vector2Int boardPosition) =>
+		new Vector3(boardPosition.x, 0f, -boardPosition.y);
+}
