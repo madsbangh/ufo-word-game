db027928144cd2030f8688af68e4dcd323c8ecb0
Mads <madsbangh@gmail.com>
2022-02-23 00:02:57 +0100

    Remove unnecessary using

--- Assets/Scripts/Components/GameController.cs
+++ Assets/Scripts/Components/GameController.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
 using SectionWords = System.Collections.Generic.Dictionary<string, (UnityEngine.Vector2Int, WordDirection)>;
 
 namespace Components
