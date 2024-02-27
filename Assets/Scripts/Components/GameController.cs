ebe5f10e4f88ee290a49f5d5906cbd044adad215
Mads <madsbangh@gmail.com>
2024-02-28 00:12:08 +0100

    Move GameState to its own file

--- Assets/Scripts/Components/GameController.cs
+++ Assets/Scripts/Components/GameController.cs
@@ -12,7 +12,7 @@ using SectionWords = System.Collections.Generic.Dictionary<string, WordPlacement
 
 namespace Components
 {
-    public class GameController : MonoBehaviour
+    public partial class GameController : MonoBehaviour
     {
         public const int HintPointsRequiredPerHint = 3;
 
@@ -494,39 +494,6 @@ namespace Components
             {
                 stream.Visit(ref Letters);
                 stream.Visit(ref Words);
-            }
-        }
-
-        private struct GameState : ISerializable
-        {
-            public Queue<Section> GeneratedFutureSections;
-            public SectionWords CurrentSectionWords;
-            public int CurrentSectionIndex;
-            public int NewestGeneratedSectionIndex;
-            public string CurrentSectionLetters;
-            public int Score;
-            public Queue<string> RecentlyFoundWords;
-            public int BonusHintPoints;
-            public bool FirstEverWordCompleted;
-
-            public void Serialize(ReadOrWriteFileStream stream)
-            {
-                stream.Visit(ref CurrentSectionIndex);
-                stream.Visit(ref NewestGeneratedSectionIndex);
-                stream.Visit(ref CurrentSectionLetters);
-                stream.Visit(ref CurrentSectionWords);
-                stream.Visit(ref GeneratedFutureSections);
-                stream.Visit(ref Score);
-                stream.Visit(ref RecentlyFoundWords);
-                stream.Visit(ref BonusHintPoints);
-
-                // Version 1.1.1 and below
-                if (stream.FileFormatVersion < 1)
-                {
-                    return;
-                }
-
-                stream.Visit(ref FirstEverWordCompleted);
             }
         }
     }
---
+++ Assets/Scripts/Components/GameState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using SaveGame;
+using SectionWords = System.Collections.Generic.Dictionary<string, WordPlacement>;
+
+namespace Components
+{
+    public partial class GameController
+    {
+        private struct GameState : ISerializable
+        {
+            public Queue<Section> GeneratedFutureSections;
+            public SectionWords CurrentSectionWords;
+            public int CurrentSectionIndex;
+            public int NewestGeneratedSectionIndex;
+            public string CurrentSectionLetters;
+            public int Score;
+            public Queue<string> RecentlyFoundWords;
+            public int BonusHintPoints;
+            public bool FirstEverWordCompleted;
+
+            public void Serialize(ReadOrWriteFileStream stream)
+            {
+                stream.Visit(ref CurrentSectionIndex);
+                stream.Visit(ref NewestGeneratedSectionIndex);
+                stream.Visit(ref CurrentSectionLetters);
+                stream.Visit(ref CurrentSectionWords);
+                stream.Visit(ref GeneratedFutureSections);
+                stream.Visit(ref Score);
+                stream.Visit(ref RecentlyFoundWords);
+                stream.Visit(ref BonusHintPoints);
+
+                // Version 1.1.1 and below
+                if (stream.FileFormatVersion < 1)
+                {
+                    return;
+                }
+
+                stream.Visit(ref FirstEverWordCompleted);
+            }
+        }
+    }
+}
